e1c0cf64602c996788bc02688ba8d3374dd4e6cb
dcd_100005679501_15594648816 <dcd_100005679501_15594648816@git.cloud.tencent.com>
2019-04-25 17:10:02 +0800

    first init

---
+++ cmake-build-debug/CMakeFiles/SimplePlayer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/person/SimplePlayer/pcm_player.c" "/cygdrive/d/person/SimplePlayer/cmake-build-debug/CMakeFiles/SimplePlayer.dir/pcm_player.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/cygwin64/usr/local/include/SDL2"
+  "C:/cygwin64/usr/local/ffmpeg/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SimplePlayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimplePlayer.dir/pcm_player.c.o"
+  "SimplePlayer.pdb"
+  "SimplePlayer.exe"
+  "libSimplePlayer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SimplePlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
